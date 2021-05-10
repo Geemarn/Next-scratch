3efd068bb613086178ad8416483d8dfc8a5451ce
Adegoke <unique>
2021-05-10 22:19:14 +0100

    render the app and get the context of the page with collected side effects

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,21 @@
+import React from 'react';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
-
+import {ServerStyleSheets} from '@material-ui/core';
 class MyDocument extends Document {
 
-  static async getInitialProps(context) {
+  static async getInitialProps(context
+  ) {
+    //render the app and get the context of the page with collected side effects
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = context.renderPage;
+    context.renderPage = () => originalRenderPage({
+      enhanceApp: App => props => sheets.collect(<App {...props} />)
+    });
     const initialProps = await Document.getInitialProps(context);
-    return {...initialProps};
+    return {...initialProps, styles: [
+        ...React.Children.toArray(initialProps.styles),
+        sheets.getStyleElement(),
+      ]};
   }
 
   render(): JSX.Element {
