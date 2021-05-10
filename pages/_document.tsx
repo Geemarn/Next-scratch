11afd3db33df53bc902fd11354fb76707baf0a15
Adegoke <unique>
2021-05-10 20:52:47 +0100

    skeleton setup

---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+
+  static async getInitialProps(context) {
+    const initialProps = await Document.getInitialProps(context);
+    return {...initialProps};
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
